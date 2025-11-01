06ab8a58978162e20188df290c22f329d92d50e8
rashidinvincible <mamoonrashid829432@gmail.com>
2025-11-01 20:40:07 +0530

    Updated entity

--- airBnbApp/src/main/java/com/mamoon/project/airBnbApp/entity/Booking.java
+++ airBnbApp/src/main/java/com/mamoon/project/airBnbApp/entity/Booking.java
@@ -10,6 +10,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -50,4 +51,11 @@ public class Booking {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private BookingStatus bookingStatus;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "booking_guest",
+               joinColumns = @JoinColumn(name = "booking_id"),
+               inverseJoinColumns = @JoinColumn(name = "guest_id")
+    )
+    private Set<Guest> guests;
 }
--- airBnbApp/src/main/java/com/mamoon/project/airBnbApp/entity/Guest.java
+++ airBnbApp/src/main/java/com/mamoon/project/airBnbApp/entity/Guest.java
@@ -1,5 +1,29 @@
 package com.mamoon.project.airBnbApp.entity;
 
 
+import com.mamoon.project.airBnbApp.entity.enums.Gender;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Guest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private Integer age;
 }
