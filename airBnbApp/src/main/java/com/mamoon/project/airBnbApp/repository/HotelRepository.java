1ae7526e6d00a3b93f8062095e4f463a3468911d
rashidinvincible <mamoonrashid829432@gmail.com>
2025-11-03 17:39:48 +0530

    Updated repository

--- airBnbApp/src/main/java/com/mamoon/project/airBnbApp/repository/HotelRepository.java
+++ airBnbApp/src/main/java/com/mamoon/project/airBnbApp/repository/HotelRepository.java
@@ -2,6 +2,8 @@ package com.mamoon.project.airBnbApp.repository;
 
 import com.mamoon.project.airBnbApp.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
 }
