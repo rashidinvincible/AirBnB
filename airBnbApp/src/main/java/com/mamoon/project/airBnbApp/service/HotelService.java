3b31a10d0e81518b1b0813709dd9ddf9f2d0e984
rashidinvincible <mamoonrashid829432@gmail.com>
2025-11-03 17:41:36 +0530

    Updated Service

--- airBnbApp/src/main/java/com/mamoon/project/airBnbApp/service/HotelService.java
+++ airBnbApp/src/main/java/com/mamoon/project/airBnbApp/service/HotelService.java
@@ -1,4 +1,12 @@
 package com.mamoon.project.airBnbApp.service;
 
+import com.mamoon.project.airBnbApp.dto.HotelDto;
+import com.mamoon.project.airBnbApp.entity.Hotel;
+
 public interface HotelService {
+
+    HotelDto createNewHotel(HotelDto hotelDto);
+
+    HotelDto getHotelById(Long id);
+
 }
--- airBnbApp/src/main/java/com/mamoon/project/airBnbApp/service/HotelServiceImpl.java
+++ airBnbApp/src/main/java/com/mamoon/project/airBnbApp/service/HotelServiceImpl.java
@@ -1,11 +1,38 @@
 package com.mamoon.project.airBnbApp.service;
 
+import com.mamoon.project.airBnbApp.dto.HotelDto;
+import com.mamoon.project.airBnbApp.entity.Hotel;
+import com.mamoon.project.airBnbApp.exception.ResourceNotFoundException;
+import com.mamoon.project.airBnbApp.repository.HotelRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class HotelServiceImpl implements HotelService {
+
+    private final HotelRepository hotelRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public HotelDto createNewHotel(HotelDto hotelDto) {
+        log.info("Creating a new hotel with name {}",hotelDto.getName());
+        Hotel hotel = modelMapper.map(hotelDto, Hotel.class);
+        hotel.setActive(false);
+        hotel = hotelRepository.save(hotel);
+        log.info("Created a new hotel with iD: {}",hotel.getId());
+        return modelMapper.map(hotel, HotelDto.class);
+    }
+
+    @Override
+    public HotelDto getHotelById(Long id) {
+        log.info("Getting hotel with id {}",id);
+        Hotel hotel = hotelRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Hotel not found with ID: " + id));
+        return modelMapper.map(hotel, HotelDto.class);
+
+    }
 }
