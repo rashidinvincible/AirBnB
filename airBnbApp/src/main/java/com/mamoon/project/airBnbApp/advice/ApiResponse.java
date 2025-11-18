27b6de37943dfe1bb41b96dd243b8f4173f521f4
rashidinvincible <mamoonrashid829432@gmail.com>
2025-11-18 20:55:09 +0530

    Added Global Response Handler

---
+++ airBnbApp/src/main/java/com/mamoon/project/airBnbApp/advice/ApiResponse.java
@@ -0,0 +1,33 @@
+package com.mamoon.project.airBnbApp.advice;
+
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ApiResponse<T> {
+
+    private LocalDateTime timeStamp;
+    private T data;
+    private ApiError error;
+
+    public ApiResponse()
+    {
+        this.timeStamp = LocalDateTime.now();
+    }
+
+    public ApiResponse(T data)
+    {
+        this();
+        this.data = data;
+    }
+    public ApiResponse(ApiError error)
+        {
+        this();
+        this.error = error;
+        }
+}
+
+
