88229eeaf518ed6e6ae9822984592a873afd1eff
rashidinvincible <mamoonrashid829432@gmail.com>
2025-11-18 20:44:55 +0530

    Updated Repository

--- airBnbApp/src/main/java/com/mamoon/project/airBnbApp/repository/InventoryRepository.java
+++ airBnbApp/src/main/java/com/mamoon/project/airBnbApp/repository/InventoryRepository.java
@@ -1,7 +1,14 @@
 package com.mamoon.project.airBnbApp.repository;
 
 import com.mamoon.project.airBnbApp.entity.Inventory;
+import com.mamoon.project.airBnbApp.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
+@Repository
 public interface InventoryRepository extends JpaRepository<Inventory,Long> {
+
+    //void deleteByDateAfterAndRoom(LocalDate date, Room room);
 }
--- airBnbApp/src/main/java/com/mamoon/project/airBnbApp/repository/RoomRepository.java
+++ airBnbApp/src/main/java/com/mamoon/project/airBnbApp/repository/RoomRepository.java
@@ -2,6 +2,8 @@ package com.mamoon.project.airBnbApp.repository;
 
 import com.mamoon.project.airBnbApp.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoomRepository extends JpaRepository<Room,Long> {
 }
